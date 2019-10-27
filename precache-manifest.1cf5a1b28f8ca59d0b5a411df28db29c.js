c2fe870602f2b7bac0bc9637651ca83c529acd7d
imfarhad <fakhrul.farhad@gmail.com>
2019-10-27 08:54:11 +0200

    Updates

---
+++ precache-manifest.1cf5a1b28f8ca59d0b5a411df28db29c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d222c40679be29d1e277f0c6c5f3d143",
+    "url": "/next-games/index.html"
+  },
+  {
+    "revision": "29aae626c4bfb3911aab",
+    "url": "/next-games/static/js/2.967a9b9e.chunk.js"
+  },
+  {
+    "revision": "e2fd61e7d7999e391724",
+    "url": "/next-games/static/js/main.b98494b4.chunk.js"
+  },
+  {
+    "revision": "b37c541cf4d3218d56f8",
+    "url": "/next-games/static/js/runtime-main.8b23566a.js"
+  }
+]);
